4d4f7618c57052e4787b3736f71b5570d249e676
Jordan Parker <parkerj4321@gmail.com>
2018-11-29 15:42:35 -0600

    Just need to figure out how to share a static message

--- app/src/main/java/com/example/student/recipebook/MainActivity.java
+++ app/src/main/java/com/example/student/recipebook/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.student.recipebook;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private ArrayList<RecipeBook> mRecipeData;
     private RecipeAdapter mAdapter;
+    private String mWebsiteEditText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
 
@@ -29,8 +40,34 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new RecipeAdapter(this, mRecipeData);
         mRecyclerView.setAdapter(mAdapter);
+        mWebsiteEditText = (String) "http://www.jpdevelops.com";
 
         initializeData();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.recipe_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "My Site", Toast.LENGTH_SHORT).show();
+                openWebsite();
+                break;
+            case R.id.item2:
+                Toast.makeText(this, "This App was created in Memphis, TN", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 Selected", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initializeData() {
@@ -48,5 +85,21 @@ public class MainActivity extends AppCompatActivity {
         }
         image.recycle();
         mAdapter.notifyDataSetChanged();
+    }
+
+    public void openWebsite() {
+        // Get the URL text.
+        String url = mWebsiteEditText;
+
+        // Parse the URI and create the intent.
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to hand the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
     }
 }
